3f97b3fa7972a3d3aba551330e6bf4d8f95c2194
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2022-03-08 09:52:15 -0400

    [UPDATED] KillBox
    Also destroys ball if it touches

--- Assets/Packages/DeathBlock/Scripts/KillBox.cs
+++ Assets/Packages/DeathBlock/Scripts/KillBox.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class KillBox : MonoBehaviour
 {
-
     public Respawn respawn;
+    public BallCollectable ballCollectable;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            respawn.activated = true;
-        }
+        if (other.gameObject.CompareTag("Player")) respawn.activated = true;
+
+        if (other.gameObject.CompareTag("Ball")) ballCollectable.ball.SetActive(false);
     }
 }
